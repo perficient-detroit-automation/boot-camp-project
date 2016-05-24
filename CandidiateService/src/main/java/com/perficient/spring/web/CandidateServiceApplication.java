21802dbff5980a8f68a7662de81555aa4e74ec09
Justin Grothe <justin.grothe@perficient.com>
2016-05-24 11:00:44 -0400

    test commit

--- CandidiateService/src/main/java/com/perficient/spring/web/CandidateServiceApplication.java
+++ CandidiateService/src/main/java/com/perficient/spring/web/CandidateServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CandidateServiceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(CandidateServiceApplication.class, args);
 	}
 }
