093b4ae8afe498e674e2648586d6b6b5009a7eb7
Justin Grothe <justin.grothe@perficient.com>
2016-06-01 16:14:41 -0400

    Was testing with the Save and Convert to employee buttons, got them linked to the controller

--- CandidiateService/src/main/java/com/perficient/spring/web/controller/HomeController.java
+++ CandidiateService/src/main/java/com/perficient/spring/web/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.perficient.spring.web.dao.CandidateService;
 
@@ -16,9 +17,21 @@ public final class HomeController {
 	private CandidateService service;
 
 
-	@RequestMapping("/")
+	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String showThymeleafPage(Model model) {
 		model.addAttribute("candidate", service.getSampleCandidate());
 		return "candidate-thymeleaf";
+	}
+	
+	@RequestMapping(value="/",params="save",method=RequestMethod.POST)
+	public String saveMethod() {
+		//Save button implementation for Candidate Service goes here
+		return "candidate-thymeleaf";
+	}
+	
+	@RequestMapping(value="/",params="convert",method=RequestMethod.POST)
+	public String convertMethod() {
+		//Convert to employee button implementation for Candidate Service goes here
+		return "home2";
 	}
 }
