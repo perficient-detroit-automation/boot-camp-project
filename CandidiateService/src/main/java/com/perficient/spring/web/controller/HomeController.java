836269b61ed4c5c276997b7370df3b3afec9e89f
Justin Grothe <justin.grothe@perficient.com>
2016-06-03 13:03:35 -0400

    Got the post method to work sort of for the save and convert to employee buttons. Working on the dropdowns now

--- CandidiateService/src/main/java/com/perficient/spring/web/controller/HomeController.java
+++ CandidiateService/src/main/java/com/perficient/spring/web/controller/HomeController.java
@@ -1,21 +1,24 @@
 package com.perficient.spring.web.controller;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.perficient.spring.web.dao.Candidate;
 import com.perficient.spring.web.dao.CandidateService;
 
 
 @Controller
 public final class HomeController {
+	
+	static Logger log = Logger.getLogger(HomeController.class);
 
 	@Autowired
 	private CandidateService service;
-
 
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String showThymeleafPage(Model model) {
@@ -23,13 +26,22 @@ public final class HomeController {
 		return "candidate-thymeleaf";
 	}
 	
-	@RequestMapping(value="/",params="save",method=RequestMethod.POST)
-	public String saveMethod() {
+	@RequestMapping(value="/method",params="save",method=RequestMethod.POST)
+	public String saveMethod(Candidate candidate) {
 		//Save button implementation for Candidate Service goes here
-		return "candidate-thymeleaf";
+		log.isInfoEnabled();
+		log.info(candidate.getFirstName());
+		log.info(candidate.getLastName());
+		log.info(candidate.getPhoneNumber());
+		log.info(candidate.getEmailAddress());
+		log.info(candidate.getDegree());
+		log.info(candidate.getStatus());
+		log.info(candidate.getGraduationDate());
+		log.info("TESTTTTTTTTTTTTTTTT");
+		return "redirect:/";
 	}
 	
-	@RequestMapping(value="/",params="convert",method=RequestMethod.POST)
+	@RequestMapping(value="/method",params="convert",method=RequestMethod.POST)
 	public String convertMethod() {
 		//Convert to employee button implementation for Candidate Service goes here
 		return "home2";
