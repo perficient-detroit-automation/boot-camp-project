6eb4b61bb5a14c048c7a2d62aca1382557bb9ba3
Justin Grothe <justin.grothe@perficient.com>
2016-06-21 14:43:20 -0400

    Got employee thymleaf page to work, had to add ModelAttributes in the controller for the dropdowns, change some of the code. You can run employee application now, it starts on port 8082. None of the database is set up and i commented out the dependencies for now

--- employee/src/main/java/com/perficient/spring/web/controller/HomeController.java
+++ employee/src/main/java/com/perficient/spring/web/controller/HomeController.java
@@ -1,14 +1,56 @@
 package com.perficient.spring.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.perficient.spring.web.model.DropdownOption;
+import com.perficient.spring.web.model.Employee;
 
 @Controller
 public class HomeController {
+	
+	@ModelAttribute("employeeType")
+	public List<DropdownOption> populateWhichType() {
+		List<DropdownOption> employeeType = new ArrayList<DropdownOption>();
+
+		employeeType.add(new DropdownOption(0, "Intern"));
+		employeeType.add(new DropdownOption(1, "Part-Time"));
+		employeeType.add(new DropdownOption(2, "Full-Time"));
+
+		return employeeType;
+	}
+	
+	@ModelAttribute("employeeDept")
+	public List<DropdownOption> populateDept() {
+		List<DropdownOption> employeeType = new ArrayList<DropdownOption>();
+
+		employeeType.add(new DropdownOption(0, "Automation"));
+		employeeType.add(new DropdownOption(1, "Development"));
+		employeeType.add(new DropdownOption(2, "Testing"));
+
+		return employeeType;
+	}
+	
+	@ModelAttribute("userRole")
+	public List<DropdownOption> populateRoles() {
+		List<DropdownOption> employeeType = new ArrayList<DropdownOption>();
+
+		employeeType.add(new DropdownOption(0, "Candidate"));
+		employeeType.add(new DropdownOption(1, "Employee"));
+		employeeType.add(new DropdownOption(2, "Administrator"));
+
+		return employeeType;
+	}
 
 	@RequestMapping(value = "/")
-	public String showThymeleafPage() {
-		return "home";
+	public String showThymeleafPage(Model model) {
+		model.addAttribute("employee", new Employee());
+		return "manage-personnel_EmpThyme";
 	}
 	
 }
--- employee/src/main/java/com/perficient/spring/web/repository/EmployeeRepository.java
+++ employee/src/main/java/com/perficient/spring/web/repository/EmployeeRepository.java
@@ -1,12 +1,13 @@
 package com.perficient.spring.web.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.perficient.spring.web.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository {
+//public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 	Employee findOne(int id);
 	Employee saveEmployee(Employee c);
