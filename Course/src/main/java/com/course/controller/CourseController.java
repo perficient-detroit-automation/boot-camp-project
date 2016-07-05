aa753b42f7f0f250556703436c71e006be7ac478
PratyushaV <vankayala.pratyusha@gmail.com>
2016-07-05 11:12:42 -0400

    Course service

---
+++ Course/src/main/java/com/course/controller/CourseController.java
@@ -0,0 +1,15 @@
+package com.course.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CourseController {
+	
+	@RequestMapping(value="/")
+	public String showHome(){
+	
+		return "sample";
+	}
+
+}
